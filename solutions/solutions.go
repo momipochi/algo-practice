74c374e0be7765125f3a24ae5203bb37061abef6
unknown <k25650727@gmail.com>
2024-06-08 10:02:54 +0200

    commit

--- solutions/solutions.go
+++ solutions/solutions.go
@@ -27,3 +27,17 @@ func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
 	prev.Next = target.Next
 	return head
 }
+
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+//https://leetcode.com/problems/maximum-subarray/
+
+func MaxSubArray(nums []int) int {
+	cMax, oMax := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		nextMax := nums[i] + cMax
+		cMax = max(nums[i], nextMax)
+		oMax = max(cMax, oMax)
+	}
+	return oMax
+
+}
--- solutions/solutions_test.go
+++ solutions/solutions_test.go
@@ -27,3 +27,14 @@ func TestRemoveNthFromEnd(t *testing.T) {
 		res = res.Next
 	}
 }
+
+func TestMaxSubArray(t *testing.T) {
+	cases := [][]int{{5, 4, -1, 7, 8}, {-2, 1, -3, 4, -1, 2, 1, -5, 4}, {1}}
+	ans := []int{23, 6, 1}
+	for i := 0; i < len(cases); i++ {
+		res := MaxSubArray(cases[i])
+		if res != ans[i] {
+			t.Fatalf("MaxSubArray, Answers do not match")
+		}
+	}
+}
